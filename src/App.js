ba7ae25ee5c49fb2d402f9c934ff542cf9fd4f54
Rawaz Ali <rawazali988@gmail.com>
2022-10-18 21:34:30 +0300

    use useContect , localStorage and add handle delete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,84 @@
 import "./App.css";
 import TodoForm from "./components/TodoForm";
 import React from "react";
-// import { useState } from "react";
+import { useState} from "react";
+import { createContext } from "react";
+import Todo from "./components/Todo";
+
+export const TodoContext = createContext();
 
 function App() {
+  const [todos, setTodos] = useState(() => {
+    // get the todos from localstorage
+    const savedTodos = localStorage.getItem("todos");
+    // if there are todos stored
+    if (savedTodos) {
+      // return the parsed JSON object back to a javascript object
+      return JSON.parse(savedTodos);
+      // otherwise
+    } else {
+      // return an empty array
+      return [];
+    }
+  } );
+  const [input, setInput] = useState("");
+
+  function addTodo(e) {
+    e.preventDefault();
+    // putting the state and the new todo to setTodos
+    setTodos(
+      todos,
+      todos.push({
+        id: Math.floor(Math.random() * 10000),
+        text: input,
+        isCompleted: false,
+      }),
+      todos.reverse()
+    );
+    console.log(todos);
+    setInput("");
+    // save the todos to local storage here instead using useEffect because we prevent the page from reloading
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+  }
+  function handleDelete(id) {
+    // filter the todos and return the todos that don't match the id
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    // set the new state
+    setTodos(newTodos);
+  }
+
+  // function handleEdit(id, text) {
+  //   // edit the objet with the id
+  //   const newTodos = todos.map((todo) => {
+  //     if (todo.id === id) {
+  //       todo.text = text;
+  //     }
+  //     return todo;
+  //   });
+  //   // set the new state
+  //   setTodos(newTodos);
+  //   console.log(todos);
+  // }
+
+  const globalStates = {
+    todos: todos,
+    input: input,
+    addTodo,
+    handleDelete,
+    setTodos,
+    setInput,
+  };
+
   return (
-    <div className="w-screen h-screen bg-cyan-100">
-      <div className="  flex justify-center items-center text-white">
+    <div className="w-screen h-screen  bg-cyan-100">
+      <div className=" flex justify-center py-10 text-white">
         <div className="flex flex-col m-4 bg-cyan-700 space-y-2 shadow rounded-lg   p-4">
-          <h1 className="mx-auto">Todo App</h1>
-          <TodoForm />
+          <h1 className="mx-auto text-xl">Todo App</h1>
+          <TodoContext.Provider value={globalStates}>
+            <TodoForm />
+            <Todo />
+          </TodoContext.Provider>
         </div>
       </div>
     </div>
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,62 +1,44 @@
 import React from "react";
-import { useState} from "react";
-import Todo from "./Todo";
-
+import { useContext } from "react";
+import { TodoContext } from "../App";
 
 export default function TodoForm() {
+  
 
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState("");
-
-  function addTodo(e) {
-    e.preventDefault();
-    // putting the state and the new todo to setTodos
-    setTodos(
-      todos,
-      todos.push({
-        id: Math.floor(Math.random() * 100),
-        text: input,
-        isCompleted: false,
-      }),todos.reverse()
-    );
-    console.log(todos);
-    setInput("");
-  }
-
+  const myContext = useContext(TodoContext);
+  
   return (
     <>
-      
-        <form className=" flex justify-center items-center">
-          <input
-            id="todoInput"
-            className="p-2 border-2 border-cyan-900 text-cyan-900 rounded-l-md focus:outline-none focus:border-cyan-300"
-            type={"text"}
-            value={input}
-            placeholder="What you want to do?"
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <button
-            onClick={addTodo}
-            className="bg-cyan-900 text-white px-4 py-2 rounded-r-md  border-2 border-cyan-900"
+      <form className=" flex justify-center items-center">
+        <input
+          id="todoInput"
+          className="p-2 border-2 w-56 border-cyan-900 text-cyan-900 rounded-l-md focus:outline-none focus:border-cyan-300"
+          type={"text"}
+          value={myContext.input}
+          placeholder="What do you want to do?"
+          onChange={(e) => myContext.setInput(e.target.value,e.preventDefault())}
+        />
+        <button
+          onClick={myContext.addTodo}
+          className="bg-cyan-900 text-white px-4 py-2 rounded-r-md  border-2 border-cyan-900"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          </button>
-        </form>
-        <hr />
-        <Todo todos={todos}/>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+      </form>
+      <hr />
     </>
   );
 }
